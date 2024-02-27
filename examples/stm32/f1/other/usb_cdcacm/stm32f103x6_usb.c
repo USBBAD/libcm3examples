bdd93a2fac11d69d3d4646043073a85e29a1be68
DmitryMurashov <dmtr.murashov@gmail.com>
2024-02-27 18:42:02 +0300

    [usb_cdcacm] Impl | Stub for custom USB implementation

---
+++ examples/stm32/f1/other/usb_cdcacm/stm32f103x6_usb.c
@@ -0,0 +1,16 @@
+//
+// stm32f103x6_usb.c
+//
+// Created on: February 27, 2024
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+// TODO: fix include guard
+
+#ifndef EXAMPLES_STM32_F1_OTHER_USB_CDCACM_STM32F103X6_USB_C_
+#define EXAMPLES_STM32_F1_OTHER_USB_CDCACM_STM32F103X6_USB_C_
+
+#include <stm32f103x6.h>
+
+#endif  // EXAMPLES_STM32_F1_OTHER_USB_CDCACM_STM32F103X6_USB_C_
+
